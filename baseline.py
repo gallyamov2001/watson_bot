f62ba43a4c2a7e37fd8894a7f2f5fcb0f881879a
gallyamov2001 <70097595+gallyamov2001@users.noreply.github.com>
2023-03-15 02:14:15 +0300

    Add files via upload

---
+++ baseline.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+from transformers import pipeline
+
+
+def get_text(url):
+    rs = requests.get(url)
+    root = BeautifulSoup(rs.content, 'html.parser')
+    article = root.select_one('article')
+
+    return article.text
+
+
+
+
+print("Введите ссылку на документацию в html формате:\n")
+url = input()
+text = get_text(url)
+
+try:
+    while True:
+        model = pipeline(model='AndrewChar/model-QA-5-epoch-RU')
+        print("Введите вопрос...\n")
+        question = input()
+        answer = model(context=text, question=question)
+        print(answer['answer'])
+
+except KeyboardInterrupt:
+    print('Finished!')
+
